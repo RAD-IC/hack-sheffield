190b0d6776b34841368449e4737681a70933478b
Daniel Boulby <db2515@ic.ac.uk>
2017-10-14 20:24:01 +0100

    Add basic link form

--- app/views/components/app/app.js
+++ app/views/components/app/app.js
@@ -1,5 +1,19 @@
 app.controller('appCtrl', function($scope, $http, $routeParams, socket, Data) {
-
-    /* Init vars */
-    $scope.variable = "foo";
+    $scope.warning = false;
+    $scope.warningMessage = '';
+    $scope.pincode = '';
+    $scope.sentData = false;
+    $scope.validData = false;
+    $scope.sendPin = () => {
+      let val = $scope.pincode;
+      let isnum = /^\d+$/.test(val);
+      if (!isnum) {
+        $scope.warning = true;
+        $scope.warningMessage = "Invalid Device Number: Device number must only contain numbers.";
+      } else {
+        $scope.warning = true;
+        $scope.warningMessage = "Invalid Device Number: Device is not registered on the system. Please connect the device to the system first.";
+        $scope.sentData = true;
+      }
+    }
 });
