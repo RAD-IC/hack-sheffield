964f6f6171c9b089c2871934b4f46d4c004cdf48
Alberto Spina <as12015@ic.ac.uk>
2017-10-15 05:44:07 +0100

    Attempt to remove duplication in socket io listeners

--- app/views/components/app/app.js
+++ app/views/components/app/app.js
@@ -20,22 +20,33 @@ app.controller('appCtrl', function($scope, $http, $routeParams, $location, socke
       }
     };
 
-    socket.on('joinFailure', () => {
-        $scope.warning = true;
-        $scope.warningMessage = "Invalid Device Number: Device is not registered on the system. Please connect the device to the system first.";
-        console.log('No device');
-        $scope.currStatus = 0;
-        $scope.$apply();
+    function joinFailure() {
+        socket.on('joinFailure', () => {
+            $scope.warning = true;
+            $scope.warningMessage = "Invalid Device Number: Device is not registered on the system. Please connect the device to the system first.";
+            console.log('No device');
+            $scope.currStatus = 0;
+            $scope.$apply();
 
-        /* Handle device not currently waiting on connection */
+            /* Handle device not currently waiting on connection */
+        });
+    }
+
+    socket.removeAllListeners('joinFailure', function() {
+        socket.once('joinFailure', joinFailure);
     });
 
-    socket.on('pollWait', (data) => {
-        $scope.currStatus = 2;
-        $scope.$apply();
-        /* Device is on the network, waiting for button presses */
-        console.log('Device present, press button');
-    });
+    function pollWait() {
+        socket.on('pollWait', (data) => {
+            $scope.currStatus = 2;
+            $scope.$apply();
+            /* Device is on the network, waiting for button presses */
+            console.log('Device present, press button');
+        });
+    }
+
+    socket.removeAllListeners('pollWait', function() {
+        socket.once('pollWait', pollWait);
     
     /* TODO set statte to 3 when the device button is pressed */
 
@@ -44,7 +55,7 @@ app.controller('appCtrl', function($scope, $http, $routeParams, $location, socke
 
     if (tmp !== 'home') {
         $scope.hash = tmp;
-        //socket.emit('joinRoom', {ID: $scope.hash});
+        socket.emit('joinRoom', {ID: $scope.hash});
         console.log(tmp);
         $location.url('/app/home');
     }
