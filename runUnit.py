ea8aaac97457836a9e0e8d268c0298bf932c0293
Riku Murai <riku.murai15@imperial.ac.uk>
2017-10-15 10:03:14 +0100

    runUnit.py: Change io port assignment

--- runUnit.py
+++ runUnit.py
@@ -17,18 +17,17 @@ subprocess.call("cd arduino_daemon && node index.js & cd ..", shell=True)
 output = subprocess.check_output("ls /dev | grep cu.usbmodem", shell=True)
 splitOutput = output.split('\n')
 splitOutput = splitOutput[:-1]
-lowestIO = 5000
+IO = 1421
 for output in splitOutput:
     num = int(output[11:])
-    print(num)
-    if(num < lowestIO):
-        lowestIO = num
+    if(num != 1411):
+        IO = num
 
 
 arduinoPath = "Arduino/"
 
 arduinoCmd = "cd " + arduinoPath + \
-    " &&  make && ./sheffield /dev/cu.usbmodem" + str(lowestIO) + \
+    " &&  make && ./sheffield /dev/cu.usbmodem" + str(IO) + \
     " " + modelNumber
 
 subprocess.call(arduinoCmd, shell=True)
