f65d8d36ca2173e590122addf9e6b2506b6e3dce
Alberto Spina <as12015@ic.ac.uk>
2017-10-15 04:49:04 +0100

    Attempt to fix socket broadcast

--- app/models/socket-io/socket-io.js
+++ app/models/socket-io/socket-io.js
@@ -91,7 +91,7 @@ exports.start = (server) => {
 
         socket.on('broadcastPress', (data) => {
             console.log('Gonna broadcast arduino press ' + data.ID);
-            io.in(data.ID).emit('arduinoPress');
+            io.sockets.in(data.ID).emit('arduinoPress')
         });
 
         /* Triggered by joining a new room */
