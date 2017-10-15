a0c282b2261177762dda61bbee5777ec13c0f884
Daniel Boulby <db2515@ic.ac.uk>
2017-10-15 06:58:03 +0100

    Include QR camera

--- app/views/components/app/app.js
+++ app/views/components/app/app.js
@@ -1,4 +1,19 @@
 app.controller('appCtrl', function($scope, $http, $routeParams, $location, socket, Data) {
+
+    let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+    scanner.addListener('scan', function (content) {
+      console.log(content);
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+    if (cameras.length > 0) {
+      scanner.start(cameras[0]);
+    } else {
+      console.error('No cameras found.');
+    }
+    }).catch(function (e) {
+      console.error(e);
+    });
+
     $scope.hash = '';
     $scope.warning = false;
     $scope.warningMessage = '';
